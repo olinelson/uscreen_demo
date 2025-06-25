ba0ad113b24f8fc9838a3f2490856ef914cd2dd5
Oli Nelson <olivernelson@hey.com>
2025-06-25 19:48:24 +1000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   config.hosts = [
-    "ucreen-test.fly.dev"     # Allow requests from example.com
+    "uscreen-test.fly.dev"     # Allow requests from example.com
   ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
