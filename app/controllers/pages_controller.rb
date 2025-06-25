541b6cfdd6b1cf70fd92d0e592a41a3565a2953a
Oli Nelson <olivernelson@hey.com>
2025-06-25 21:16:41 +1000

    fix

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   allow_unauthenticated_access
 
   def index
-    @offers = Offer.where(status: :published).order(created_at: :descending)
+    @offers = Offer.where(status: :published).order(created_at: :desc)
   end
 end
